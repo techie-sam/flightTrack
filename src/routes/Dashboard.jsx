e0f59e6465019deac3f8c63d0a363891655aa24e
TECHIE SAM <blessingabolade15@gmail.com>
2023-04-26 17:30:39 +0100

    added faicion

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -11,35 +11,41 @@ const Dashboard = () => {
         setData(res.data)
         console.log(data)
       })
-      .catch(() => {
+      .catch((err) => {
+        console.log(err)
         setIsLoading(false)
       })
-    // return () => {
-    // }
+    return () => {
+      null
+    }
   }, [data])
   return (
 
-    <>
-      <table className="table-auto text-center bg-[#3c92fa] w-full text-sm text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-[#3c92fa] dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th className="px-6 py-3 text-white">Airport</th>
-            <th className="px-6 py-3 text-white">TIme</th>
-            <th className="px-6 py-3 text-white">Arriving</th>
-            <th className="px-6 py-3 text-white">Departing</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <td className="px-6 py-4">Airpost1</td>
-            <td className="px-6 py-4">Time1</td>
-            <td className="px-6 py-4">Arriving1</td>
-            <td className="px-6 py-4">Departing</td>
-          </tr>
-        </tbody>
-      </table>
-
-    </>
+    <div className="bg-[#e7edf4] h-screen pt-10">
+      {
+        isLoading ? "Loading" :
+          <div className="m-auto relative overflow-x-auto shadow-md w-full md:w-[80vw] sm:rounded-lg">
+            <table className="table-auto text-center bg-[#3c92fa] text-sm text-gray-500 dark:text-gray-400 w-full">
+              <thead className="text-xs text-gray-700 uppercase bg-[#3c92fa] dark:bg-gray-700 dark:text-gray-400">
+                <tr className="text-white">
+                  <th className="py-3">Airport</th>
+                  <th className="py-3">Time</th>
+                  <th className="py-3">Arriving</th>
+                  <th className="py-3">Departing</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td className="py-4">Airport1</td>
+                  <td className="py-4">Time1</td>
+                  <td className="py-4">Arriving1</td>
+                  <td className="py-4">Departing</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+      }
+    </div>
   )
 }
 
